6aef975923913362f16a59e1b947257f20079164
Matt Hastings <matthastings85@gmail.com>
2022-03-03 06:52:30 -0600

    fixed loader transitions

--- portfolio/src/utilities/loader.js
+++ portfolio/src/utilities/loader.js
@@ -1,15 +1,15 @@
-export const navDelay = 1000;
+export const navDelay = 500;
 
-export const homeDelay = 2000;
+export const homeDelay = 1000;
 
-export const welcomeDelay = 4000;
+export const welcomeDelay = 3000;
 
-export const aboutDelay = 5000;
+export const aboutDelay = 2000;
 
-export const projectsDelay = 5100;
+export const projectsDelay = 2100;
 
-export const contactDelay = 5200;
+export const contactDelay = 2200;
 
-export const footerDelay = 5300;
+export const footerDelay = 2300;
 
-export const transition = 500;
+export const transition = 100;
